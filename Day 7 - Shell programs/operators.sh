25eb7e8145c53b78e87b9836a00160872d832780
poojavidya77 <poojavidya77@gmail.com>
2024-06-14 00:28:40 +0530

    commit

---
+++ Day 7 - Shell programs/operators.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+num1=200
+num2=200
+
+result1=$(($num1+$num2))
+echo "result od add is $result1"
+
+result2=$(($num1*$num2))
+echo "result of multiplication is $result2"
---
+++ Day 7 - Shell programs/userinput.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+read num1
+echo "the number is $num1"
+
+read -p "enter number 2: " num2
+echo "number is $num2"
